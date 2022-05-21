3db81d468142b1012b643bc3503ea60a4d7d0dc6
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-05-22 00:40:52 +0600

    first commit

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBDOz125LLyDRRIsDfYgtiZUYXbCr8eNHc",
+    authDomain: "bicycle-parts.firebaseapp.com",
+    projectId: "bicycle-parts",
+    storageBucket: "bicycle-parts.appspot.com",
+    messagingSenderId: "388693190955",
+    appId: "1:388693190955:web:7a202e7d64bb78da067d6e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
